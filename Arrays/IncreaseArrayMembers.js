1689a6524adcf8addd9078e93a4b89512b3aeae4
Nikolay Penchev <nikolay.pen4ev@gmail.com>
2015-06-02 20:48:39 +0300

    Create IncreaseArrayMembers.js

---
+++ Arrays/IncreaseArrayMembers.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Penchevi's computer on 01-Jun-15.
+ */
+//Problem 1. Increase array members
+//
+//Write a script that allocates array of 20 integers and initializes each element by its index multiplied by 5.
+//Print the obtained array on the console.
+
+var arr = new Array(20);
+for(var i = 0;i<arr.length;i++){
+    arr[i]=i*5;
+}
+console.log(arr);
